78ccaf9a61bda959ba524b5768bd2bf12debb623
CelestialRiot <33481415+CelestialRiot@users.noreply.github.com>
2017-11-26 01:14:46 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./package.json");
-
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setGame(`on ${client.guilds.size} servers`);
@@ -16,378 +15,162 @@ client.on("guildDelete", guild => {
 });
 
 client.on("message", async message => {
-
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
-
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+// all of users' data
+// usr[0] gives a user's details (user 0), change '0' to change user
+// user[0][0] gives user 0's name
+// user[0][1] gives user 0's id number
+// user[0][2] gives user 0's entire timetable
+// user[0][2][0] gives user 0's monday timetable, from monday[0] to friday[4]
+//0=yehya 1=dorvan 2=anthony 3=bryan 4=george 5=zein 6=jackie 7=peter 8=hassan 9=oliver
+const usr =
+  [
+    /*  YEHYA A. 0 */
+    ['yehya','364940154927972353',
+      [
+        'REL/MM3/ENG/ENG/SPM',
+        'REL/REL/PHS/PHS/CHM',
+        'SPM/SPM/PHS/CHM/REL',
+        'SPM/ENG/MM3/CHM/CHM',
+        'MM3/MM3/ASS/ENG/PHS',
+      ],
+    ],
+    /*  DORVAN J. 1 */
+    ['dorvan','246558516553187328',
+      [
+        'VSC/ENG/REL/REL/CMP',
+        'VSC/VSC/VET/VET/GMA',
+        'CMP/CMP/VET/GMA/VSC',
+        'CMP/REL/ENG/GMA/GMA',
+        'ENG/ENG/ASS/REL/VET',
+      ],
+    ],
+    /*  ANTHONY G. 2  */
+    ['anthony','246547506056527873',
+      [
+        'REL/MM3/ENG/ENG/CMP',
+        'REL/REL/PHS/PHS/CHM',
+        'CMP/CMP/PHS/CHM/REL',
+        'CMP/ENG/MM3/CHM/CHM',
+        'MM3/MM3/ASS/ENG/PHS',
+      ],
+    ],
+    /*  BRYAN G. 3  */
+    ['bryan','360035848902934538',
+      [
+        'VSC/REL/ENG/ENG/CMP',
+        'VSC/VSC/PHS/PHS/GMA',
+        'CMP/CMP/PHS/GMA/VSC',
+        'CMP/ENG/REL/GMA/GMA',
+        'REL/REL/ASS/ENG/PHS',
+      ],
+    ],
+    /*  GEORGE A. 4 */
+    ['george','245588462797848576',
+      [
+        'REl/ENG/BSM/BSM/CMP',
+        'REL/REL/PHS/PHS/GMA',
+        'CMP/CMP/PHS/GMA/REL',
+        'CMP/BSM/ENG/GMA/GMA',
+        'ENG/ENG/ASS/BSM/PHS',
+      ],
+    ],
+    /*  ZEIN W. 5 */
+    ['zein','378463394451161090',
+      [
+        'HLT/REL/ENG/ENG/CMP',
+        'HLT/HLT/PHS/PHS/GMA',
+        'CMP/CMP/PHS/GMA/HLT',
+        'CMP/ENG/REL/GMA/GMA',
+        'REL/REL/ASS/ENG/PHS',
+      ],
+    ],
+    /*  JACKIE S. 6 */
+    ['jackie',['150425470255628289'],
+      [
+        'LIT/MM1/REL/REL/LGL',
+        'LIT/LIT/BIO/BIO/PSY',
+        'LGL/LGL/BIO/PSY/LIT',
+        'LGL/REL/MM1/PSY/PSY',
+        'MM1/MM1/ASS/REL/BIO',
+      ],
+    ],
+    /*  PETER M. 7  */
+    ['peter','245794606208253952',
+      [
+        'VSC/ENG/REL/REL/CMP',
+        'VSC/VSC/PHS/PHS/GMA',
+        'CMP/CMP/PHS/GMA/VSC',
+        'CMP/REL/ENG/GMA/GMA',
+        'ENG/ENG/ASS/REL/PHS',
+      ],
+    ],
+    /*  HASSAN M. 8 */
+    ['hassan','358438947032006657',
+      [
+        'REL/MM3/ENG/ENG/SPM',
+        'REL/REL/BIO/BIO/CHM',
+        'SPM/SPM/BIO/CHM/REL',
+        'SPM/ENG/MM3/CHM/CHM',
+        'MM3/MM3/ASS/ENG/BIO',
+      ],
+    ],
+    /*  OLIVER S. 9 */
+    ['oliver','311756864897417217',
+      [
+        'LIT/MM1/ENG/ENG/REL',
+        'LIT/LIT/PHS/PHS/BSM',
+        'REL/REL/PHS/BSM/LIT',
+        'REL/ENG/MM1/BSM/BSM',
+        'MM1/MM1/ASS/ENG/PHS',
+      ],
+    ],
+  ];
+
+  const days = ['monday','tuesday','wednesday','thursday','friday'];
+  const id = message.author.id;
+  for (var i = 0; i < usr.length; i++) if (usr[i][1] === id) const indx = i
+  const name = usr[indx][0];
+
+  /* utility commands */
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  };
+  if (command === "myid") {
+    message.channel.send(name+': '+id);
+  };
+  if (command === "check") {
+    const id = message.author.id;
+    if (name === 'yehya') {
+      message.channel.send("alive, thanks to you, father");
+    }
+    else {
+      message.channel.send("alive");
+    };
+  };
+  /* misc commands */
   if (command === "say") {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{});
     message.channel.send(sayMessage);
   };
-
-  if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-
-  if (command === "check") {
-    const who = message.author.id;
-    if (who === '364940154927972353') {
-      message.channel.send("alive, thanks to you, father");
-    }
-    else {
-      message.channel.send("alive");
-    }
+  /* timetable commands */
+  if (command === name) {
+   const req = args.join(" ");
+    for (var i = 0; i < days.length; i++) {
+      if (days[i] === req) {
+				const day = i;
+				message.channel.send(usr[indx][2][day]);
+			}
+			else {
+				message.channel.send("give your name and a day only")
+			};
+		};
   };
-
-  if (command === "givemeyourid" || command === "ineedit") {
-    const who = message.author.id;
-    message.channel.send(who);
-  };
-
-  if (command === "thank" || command === "thanks") {
-    message.channel.send("you're welcome :3");
-  };
-
-  if (command === "iloveyoutoo") {
-    message.channel.send("<3");
-  };
-
-  if (command === "sorry") {
-    message.channel.send("learn to forgive");
-  }
-
-  if (command === "whatismypurpose") {
-
-    const who = message.author.id;
-    /*    YEHYA A.    364940154927972353    */
-    /*    DORVAN J.    246558516553187328    */
-    /*    BRYAN G.    360035848902934538    */
-    /*    ANTHONY G.    246547506056527873    */
-    /*    GEORGE A.    245588462797848576    */
-
-    const d = new Date();
-    const days = d.getDay();
-    const hours = d.getHours();
-    const hour = hours + 11;
-    const day = days;
-    message.channel.send(day);
-    if (hour > 23) {
-      hour = 0;
-      day = days + 1;
-    };
-    if (hour >= 15 && day < 7) {
-      day = days + 1;
-    };
-    if (hour >= 15 && day === 7) {
-      day = 1;
-    };
-
-    message.channel.send(hours+','+hour,+','+day+',');
-
-    if (day === 1) {
-      if (who === '364940154927972353') {
-        message.channel.send("REL/MM3/ENG/ENG/SPM"); //yehya
-      };
-      if (who === '246558516553187328') {
-        message.channel.send("VSC/ENG/REL/REL/CMP"); //dorvan
-      };
-      if (who === '360035848902934538') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //bryan
-      };
-      if (who === '246547506056527873') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //anthony
-      };
-      if (who === '245588462797848576') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //george
-      };
-    }
-    else if (day === 2) {
-      if (who === '364940154927972353') {
-        message.channel.send("REL/REL/PHS/PHS/CHM"); //yehya
-      };
-      if (who === '246558516553187328') {
-        message.channel.send("VSC/VSC/VET/VET/GMA"); //dorvan
-      };
-      if (who === '360035848902934538') {
-        message.channel.send("VSC/VSC/PHS/PHS/GMA"); //bryan
-      };
-      if (who === '246547506056527873') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //anthony
-      };
-      if (who === '245588462797848576') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //george
-      };
-    }
-    else if (day === 3) {
-      if (who === '364940154927972353') {
-        message.channel.send("SPM/SPM/PHS/CHM/REL"); //yehya
-      };
-      if (who === '246558516553187328') {
-        message.channel.send("CMP/CMP/VET/GMA/VSC"); //dorvan
-      };
-      if (who === '360035848902934538') {
-        message.channel.send("CMP/CMP/PHS/GMA/VSC"); //bryan
-      };
-      if (who === '246547506056527873') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //anthony
-      };
-      if (who === '245588462797848576') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //george
-      };
-    }
-    else if (day === 4) {
-      if (who === '364940154927972353') {
-        message.channel.send("SPM/ENG/MM3/CHM/CHM"); //yehya
-      };
-      if (who === '246558516553187328') {
-        message.channel.send("CMP/REL/ENG/GMA/GMA"); //dorvan
-      };
-      if (who === '360035848902934538') {
-        message.channel.send("CMP/ENG/REL/GMA/GMA"); //bryan
-      };
-      if (who === '246547506056527873') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //anthony
-      };
-      if (who === '245588462797848576') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //george
-      };
-    }
-    else if (day === 5) {
-      if (who === '364940154927972353') {
-        message.channel.send("MM3/MM3/ASS/ENG/PHS"); //yehya
-      };
-      if (who === '246558516553187328') {
-        message.channel.send("ENG/ENG/ASS/REL/VET"); //dorvan
-      };
-      if (who === '360035848902934538') {
-        message.channel.send("REL/REL/ASS/ENG/PHS"); //bryan
-      };
-      if (who === '246547506056527873') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //anthony
-      };
-      if (who === '245588462797848576') {
-        message.channel.send("VSC/REL/ENG/ENG/CMP"); //george
-      };
-    }
-    else {
-      message.channel.send("you pass butter");
-    };
-  };
-
-//////////////
-
-  /*  YEHYA A.  */
-  if (command === "yehya") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("REL/MM3/ENG/ENG/SPM");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("REL/REL/PHS/PHS/CHM");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("SPM/SPM/PHS/CHM/REL");
-    }
-    else if (day === "thursday") {
-      message.channel.send("SPM/ENG/MM3/CHM/CHM");
-    }
-    else if (day === "friday") {
-      message.channel.send("MM3/MM3/ASS/ENG/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  GEORGE A. */
-  if (command === "joji") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("REl/ENG/BSM/BM1/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("REL/REL/PHS/PHS/GMA");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/PHS/GMA/REL");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/BSM/ENG/GMA/GMA");
-    }
-    else if (day === "friday") {
-      message.channel.send("ENG/ENG/ASS/BMA/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  ZEIN W. */
-  if (command === "zeus") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("HLT/REL/ENG/ENG/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("HLT/HLT/PHS/PHS/GMA");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/PHS/GMA/HLT");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/ENG/REL/GMA/GMA");
-    }
-    else if (day === "friday") {
-      message.channel.send("REL/REL/ASS/ENG/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  BRYAN G.  */
-  if (command === "bryan") {
-    const day = args.join(" ");
-    if (day === "monday") {
-    message.channel.send("VSC/REL/ENG/ENG/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("VSC/VSC/PHS/PHS/GMA");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/PHS/GMA/VSC");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/ENG/REL/GMA/GMA");
-    }
-    else if (day === "friday") {
-      message.channel.send("REL/REL/ASS/ENG/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  HASSAN M. */
-  if (command === "huss") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("REL/MM3/ENG/ENG/SPM");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("REL/REL/BIO/BIO/CHM");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("SPM/SPM/BIO/CHM/REL");
-    }
-    else if (day === "thursday") {
-      message.channel.send("SPM/ENG/MM3/CHM/CHM");
-    }
-    else if (day === "friday") {
-      message.channel.send("MM3/MM3/ASS/ENG/BIO");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  JAQUIE S. */
-  if (command === "jaq") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("LIT/MM1/REL/REL/LGL");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("LIT/LIT/BIO/BIO/PSY");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("LGL/LGL/BIO/PSY/LIT");
-    }
-    else if (day === "thursday") {
-      message.channel.send("LGL/REL/MM1/PSY/PSY");
-    }
-    else if (day === "friday") {
-      message.channel.send("MM1/MM1/ASS/REL/BIO");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  DROVAN J. */
-  if (command === "dov") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("VSC/ENG/REL/REL/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("VSC/VSC/VET/VET/GMA");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/VET/GMA/VSC");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/REL/ENG/GMA/GMA");
-    }
-    else if (day === "friday") {
-      message.channel.send("ENG/ENG/ASS/REL/VET");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  ANTHONY G. */
-  if (command === "anth") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("REL/MM3/ENG/ENG/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("REL/REL/PHS/PHS/CHM");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/PHS/CHM/REL");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/ENG/MM3/CHM/CHM");
-    }
-    else if (day === "friday") {
-      message.channel.send("MM3/MM3/ASS/ENG/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-  /*  PETER M. */
-  if (command === "peter") {
-    const day = args.join(" ");
-    if (day === "monday") {
-      message.channel.send("VSC/ENG/REL/REL/CMP");
-    }
-    else if (day === "tuesday") {
-      message.channel.send("VSC/VSC/PHS/PHS/GMA");
-    }
-    else if (day === "wednesday") {
-      message.channel.send("CMP/CMP/PHS/GMA/VSC");
-    }
-    else if (day === "thursday") {
-      message.channel.send("CMP/REL/ENG/GMA/GMA");
-    }
-    else if (day === "friday") {
-      message.channel.send("ENG/ENG/ASS/REL/PHS");
-    }
-    else {
-      message.channel.send("give your name and a day (eg. rob tuesday)")
-    };
-  };
-
-});
 
 client.login(process.env.BOT_TOKEN);
