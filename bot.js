8d948a588e8b765102b3c2fc4a754101c3400ecf
CelestialRiot <33481415+CelestialRiot@users.noreply.github.com>
2017-11-26 20:14:06 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,6 +225,7 @@ const usr =
     var day = d.getDay()-1;
     const hour = d.getHours();
     const err = 'you pass butter';
+    message.channel.send(day+','+hour);
     if(day > days.length || day < days.length) message.channel.send(err)
     else if(usr[indx][1] === id) {
       var txt = usr[indx][2][day];
