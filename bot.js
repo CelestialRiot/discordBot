f246a399feb4a529151582668947bad8168f1ba2
CelestialRiot <33481415+CelestialRiot@users.noreply.github.com>
2018-03-13 22:36:49 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,9 +18,8 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   let oldUserChannel = oldMember.voiceChannel;
   var channel = client.channels.get('385344314362036224');
   if(oldUserChannel === undefined && newUserChannel !== undefined) {
-    bot.users.fetch("364940154927972353").then(myUser => {
-      //channel.sendMessage(myUser.avatarURL());
-      channel.sendMessage('hi');
+    bot.users.fetch(364940154927972353).then(myUser => {
+      channel.sendMessage(myUser.avatarURL());
     });
     //const usr = client.users.get("364940154927972353");
     //channel.sendMessage(usr);
